e237983403440faddb29da652a68167c214d0797
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-26 21:56:56 +0100

    Worked on the client logo component

--- src/components/ClientLogo/index.tsx
+++ src/components/ClientLogo/index.tsx
@@ -1,14 +1,51 @@
+import { useRef, useEffect, useState } from "react";
+import { useIntersection } from "react-use";
+import { motion } from "framer-motion";
+
 interface ClientLogoProps {
   imagePath: string;
   brandName: string;
 }
 
 const ClientLogo: React.FC<ClientLogoProps> = ({ imagePath, brandName }) => {
+  const ref = useRef(null);
+  const refIntersection = useIntersection(ref, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.2,
+  });
+  const [inView, setInView] = useState(-1);
+
+  useEffect(() => {
+    setInView(inView + 1);
+    console.log(inView);
+  }, [refIntersection]);
+
   return (
-    <div className="w-full">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1, transition: {delay: 0.3, duration: 0.4} }}
+      //   ref={ref}
+      //   initial={{ opacity: 0, scale: 0.95 }}
+      //   animate={
+      //     inView <= 1
+      //       ? { opacity: 0, scale: 0.95 }
+      //       : {
+      //           opacity: 1,
+      //           scale: 1,
+      //           transition: {
+      //             ease: [0.43, 0.13, 0.23, 0.96],
+      //             duration: 1,
+      //           },
+      //         }
+      //   }
+      className="w-full"
+    >
       <img className="w-full" src={imagePath} />
-      <p className="text-[22px] text-center lg:text-left xl:text-[28px] font-medium mt-[8px]">{brandName}</p>
-    </div>
+      <p className="text-[22px] text-center lg:text-left xl:text-[28px] font-medium mt-[8px]">
+        {brandName}
+      </p>
+    </motion.div>
   );
 };
 
