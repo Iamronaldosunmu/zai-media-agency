5db64b3ece488aaaab13fd85b4e94e0e91aa68be
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-27 10:24:44 +0100

    Reduced the time that it takes for the loader to finish loading

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -3,17 +3,23 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Loader = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   const [animationCompleted, setAnimationCompleted] = useState(false);
   return (
-    <motion.main animate={{opacity: animationCompleted ? 0 : 1}} className=" bg-white absolute top-0 right-0 left-0 bottom-0 flex items-center justify-center">
+    <motion.main
+      animate={{ opacity: animationCompleted ? 0 : 1 }}
+      className=" bg-white absolute top-0 right-0 left-0 bottom-0 flex items-center justify-center"
+    >
       <motion.img
-              onAnimationComplete={() => { setAnimationCompleted(true); setTimeout(() => navigate("/home"), 500)}}
+        onAnimationComplete={() => {
+          setAnimationCompleted(true);
+          setTimeout(() => navigate("/home"), 500);
+        }}
         initial={{ opacity: 0 }}
         animate={{
           opacity: [0, 1, 1, 1],
-          scale: [0.9, 1.2, 0.9, 1.2, 0.9, 1.2, 0.9, 1.2, 1.1],
-          transition: { duration: 5 },
+          scale: [0.9, 1.2, 0.9, 1.2, 1.1],
+          transition: { duration: 2.5 },
         }}
         className="lg:w-[200px]"
         src="/whitelogo.png"
