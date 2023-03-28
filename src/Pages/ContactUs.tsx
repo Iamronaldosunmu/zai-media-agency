f3f7a9ba7efbad0ee24abca03e2e656b36bdb429
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-28 07:07:05 +0100

    Added some state for form validation

--- src/Pages/ContactUs.tsx
+++ src/Pages/ContactUs.tsx
@@ -3,16 +3,111 @@ import SelectInputGroup from "../components/SelectInputGroup";
 import { motion } from "framer-motion";
 import { useForm, ValidationError } from "@formspree/react";
 import { toast } from "react-toastify";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Joi from "joi";
 
 const ContactUs = () => {
   const [state, handleSubmit] = useForm("xbjeabvg");
+  const [name, setName] = useState("");
+  const [brandName, setBrandName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [brandNiche, setBrandNiche] = useState("");
+  const [servicesNeeded, setServicesNeeded] = useState("");
+  const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    brandName: "",
+    brandNiche: "",
+    servicesNeeded: "",
+    message: "",
+  });
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault();
 
+    if (
+      !errors.name &&
+      !errors.email &&
+      !errors.phoneNumber &&
+      !errors.brandName &&
+      name &&
+      brandName &&
+      email &&
+      phoneNumber &&
+      brandNiche &&
+      servicesNeeded
+    ) {
+      handleSubmit(e);
+    }
+  };
   useEffect(() => {
+    console.log(state);
     if (state.succeeded) {
       toast("Form Submitted Successfully!");
+      setName("");
+      setBrandName("");
+      setBrandNiche("");
+      setEmail("");
+      setMessage("");
+      setPhoneNumber("");
+      setServicesNeeded("");
     }
-  }, [state.succeeded]);
+  }, [state.submitting]);
+
+  const validateName = (value: string) => {
+    const schema = Joi.object({
+      name: Joi.string().min(5).max(30).required().label("Name"),
+    });
+
+    const { error } = schema.validate({ name: value });
+    if (error) setErrors({ ...errors, name: error.details[0].message });
+    else {
+      setErrors({ ...errors, name: "" });
+    }
+  };
+
+  const validateBrandName = (value: string) => {
+    const schema = Joi.object({
+      brandName: Joi.string().min(5).max(30).required().label("Brand Name"),
+    });
+
+    const { error } = schema.validate({ brandName: value });
+    if (error) setErrors({ ...errors, brandName: error.details[0].message });
+    else {
+      setErrors({ ...errors, brandName: "" });
+    }
+  };
+
+  const validatePhoneNumber = (value: string) => {
+    const schema = Joi.object({
+      phoneNumber: Joi.number().required().label("Phone Number"),
+    });
+
+    const { error } = schema.validate({ phoneNumber: value });
+    if (error) setErrors({ ...errors, phoneNumber: error.details[0].message });
+    else {
+      setErrors({ ...errors, phoneNumber: "" });
+    }
+  };
+
+  const validateEmailAddress = (value: string) => {
+    const schema = Joi.object({
+      email: Joi.string()
+        .email({ tlds: { allow: false }, minDomainSegments: 2 })
+        .min(5)
+        .max(30)
+        .required()
+        .label("Email"),
+    });
+
+    const { error } = schema.validate({ email: value });
+    if (error) setErrors({ ...errors, email: error.details[0].message });
+    else {
+      setErrors({ ...errors, email: "" });
+    }
+  };
 
   return (
     <motion.main
@@ -33,24 +128,57 @@ const ContactUs = () => {
         <p className="text-[20px]">zaimediaagency@gmail.com</p>
       </section>
       <form
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-[10px] lg:gap-[30px]"
+        onSubmit={handleFormSubmit}
+        className="flex flex-col gap-[10px] lg:gap-[30px] mt-[10px]"
       >
         <div className="flex flex-col lg:flex-row w-full justify-between gap-[10px] lg:gap-[40px]">
-          <InputGroup name="name" label="Name" />
-          <InputGroup name="brandName" label="Name Of Brand" />
+          <InputGroup
+            error={errors.name}
+            setValue={setName}
+            value={name}
+            name="name"
+            validateField={validateName}
+            label="Name"
+          />
+          <InputGroup
+            error={errors.brandName}
+            setValue={setBrandName}
+            value={brandName}
+            name="brandName"
+            label="Name Of Brand"
+            validateField={validateBrandName}
+          />
         </div>
         <div className="flex flex-col lg:flex-row  w-full justify-between gap-[10px] lg:gap-[40px]">
-          <InputGroup name="email" label="Email Address" />
-          <InputGroup name="phoneNumber" label="Phone Number" />
+          <InputGroup
+            error={errors.email}
+            setValue={setEmail}
+            value={email}
+            validateField={validateEmailAddress}
+            name="email"
+            label="Email Address"
+          />
+          <InputGroup
+            validateField={validatePhoneNumber}
+            error={errors.phoneNumber}
+            setValue={setPhoneNumber}
+            value={phoneNumber}
+            name="phoneNumber"
+            label="Phone Number"
+          />
         </div>
         <div className="flex flex-col lg:flex-row w-full justify-between gap-[10px] lg:gap-[40px]">
           <SelectInputGroup
+            value={brandNiche}
             name="brandNiche"
             label="Brand Niche"
             options={["Beauty", "Fashion", "Lifestyle"]}
+            setValue={setBrandNiche}
+            error={errors.brandNiche}
           />
           <SelectInputGroup
+            setValue={setServicesNeeded}
+            value={servicesNeeded}
             name="servicesNeeded"
             label="Services Needed"
             options={[
@@ -59,19 +187,25 @@ const ContactUs = () => {
               "Content Creation",
               "TikTok Management",
             ]}
+            error={errors.servicesNeeded}
           />
         </div>
         <div>
           <div className="flex flex-col gap-[8px] w-full">
             <label className="font-bold text-[18px]">Message</label>
-            <textarea name="message" className="w-full min-h-[150px] resize-none p-[8px] border border-black bg-transparent focus:outline-none" />
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              name="message"
+              className="w-full min-h-[150px] resize-none p-[8px] border border-black bg-transparent focus:outline-none"
+            />
           </div>
         </div>
         <motion.button
           whileTap={{ scale: 0.95 }}
           className="w-full text-white bg-black h-[50px] font-bold mt-[15px] lg:mt-0"
         >
-          SUBMIT
+          {state.submitting ? "SUBMITTING..." : "SUBMIT"}
         </motion.button>
       </form>
     </motion.main>
