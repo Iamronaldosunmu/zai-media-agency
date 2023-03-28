bf4daf7531d818c3c323a9ba7466b3477d5b88b2
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-03-28 07:07:45 +0100

    Added errors and made the component a controlled component

--- src/components/InputGroup/index.tsx
+++ src/components/InputGroup/index.tsx
@@ -1,13 +1,38 @@
+import { Dispatch, SetStateAction } from "react";
+
 interface InputGroupProps {
-    label: string;
-    name: string;
+  label: string;
+  name: string;
+  error: string;
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
+  validateField: (value: string) => void;
 }
 
-const InputGroup: React.FC<InputGroupProps> = ({ label, name }) => {
+const InputGroup: React.FC<InputGroupProps> = ({
+  label,
+  name,
+  error,
+  value,
+  setValue,
+  validateField,
+}) => {
   return (
-    <div className="flex flex-col gap-[8px] w-full">
-      <label htmlFor={name} className="font-bold text-[18px]">{label}</label>
-      <input id={name} name={name} className="w-full p-[8px] border border-black bg-transparent focus:outline-none" />
+    <div className="flex flex-col  w-full">
+      <label htmlFor={name} className="font-bold text-[18px] mb-[8px]">
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        value={value}
+        onChange={(e) => {
+            setValue(e.target.value);
+            validateField(e.target.value)
+        }}
+        className="w-full p-[8px] border border-black bg-transparent focus:outline-none"
+      />
+      {error && <p className="text-[red] text-[14px]">{error}</p>}
     </div>
   );
 };
--- src/components/SelectInputGroup/index.tsx
+++ src/components/SelectInputGroup/index.tsx
@@ -1,17 +1,40 @@
+import { Dispatch, SetStateAction } from "react";
+
 interface SelectInputGroupProps {
   label: string;
-    options: string[];
-    name: string;
+  options: string[];
+  name: string;
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
+  error: string;
 }
 
-const SelectInputGroup: React.FC<SelectInputGroupProps> = ({ label, options, name }) => {
+const SelectInputGroup: React.FC<SelectInputGroupProps> = ({
+  label,
+  options,
+  name,
+  value,
+  setValue,
+  error,
+}) => {
   return (
-    <div className="flex flex-col gap-[8px] w-full">
-      <label htmlFor="name" className="font-bold">{label}</label>
-          <select id={name} name={name} className="w-full p-[10px] border border-black bg-transparent focus:outline-none">
-              <option></option>
-              {options.map(option => <option value={option}>{option}</option>)}
+    <div className="flex flex-col w-full">
+      <label htmlFor="name" className="font-bold">
+        {label}
+      </label>
+      <select
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+        id={name}
+        name={name}
+        className="w-full p-[10px] border border-black bg-transparent focus:outline-none"
+      >
+        <option></option>
+        {options.map((option) => (
+          <option value={option}>{option}</option>
+        ))}
       </select>
+      {error && <p className="text-[red] text-[14px]">{error}</p>}
     </div>
   );
 };
